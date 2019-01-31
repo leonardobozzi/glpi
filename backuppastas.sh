7ecfdfc025b773ac60fc7ca6f70ef72d35e2ea88
Leonardo Bozzi <30551758+leonardobozzi@users.noreply.github.com>
2019-01-31 13:13:32 -0200

    Acerto
    Acerto de caminho de pastas e remoção de áreas desnecessárias no código

--- backuppastas.sh
+++ backuppastas.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
 #Variaveis
-origem="$HOME/public_html/processoseletivo/"
+origem="$HOME/public_html/"
 destino="$HOME/bkp/arq_site/"          #Diretório para salvar arquivos de backup
-nw=$(date "+%Y%m%d")                 #Buscar pela data
-nb=10                                #número de cópias do banco de dados
-echo "Realizando backup dos Arquivos para Outra pasta na Hospedagem"
+nw=$(date "+%d%m%Y")                 #Buscar pela data
+
+echo "Realizando backup dos Arquivos para Outra pasta"
 cp -R $origem $destino
 
 #-------FIM DO SCRIPT
